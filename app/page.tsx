144ad69be0d3ea98efd14b9d34273828d9cd4171
iamhectorsosa <hello@hectorsosa.me>
2024-02-28 19:09:35 +0100

    Updating reviews

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import { Reviews } from "@/components/Reviews";
 async function getReviews(): Promise<typeof staticReviews> {
   if (process.env.NODE_ENV === "production") {
     const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/details/json?placeid=${process.env.GOOGLE_MAPS_PLACE_ID}&key=${process.env.GOOGLE_MAPS_KEY}`,
+      `https://places.googleapis.com/v1/places/${process.env.GOOGLE_MAPS_PLACE_ID}?fields=reviews,userRatingCount,rating&languageCode=en&key=${process.env.GOOGLE_MAPS_KEY}`,
       {
         next: { revalidate: 86400 },
       }
@@ -31,8 +31,8 @@ async function getReviews(): Promise<typeof staticReviews> {
         return staticReviews;
       });
 
-    if (response?.result?.reviews) {
-      return response.result.reviews;
+    if (response?.reviews) {
+      return response;
     }
     return staticReviews;
   }
@@ -41,7 +41,7 @@ async function getReviews(): Promise<typeof staticReviews> {
 }
 
 export default async function Home() {
-  const reviews = await getReviews();
+  const { reviews } = await getReviews();
 
   return (
     <div>
@@ -164,8 +164,8 @@ export default async function Home() {
           <div className="flex gap-y-4 flex-col justify-center items-center text-center max-w-3xl">
             <Highlight>Reviews</Highlight>
             <h1 className="text-xl lg:text-3xl font-headings">
-              I am dedicated to helping individuals of all fitness levels
-              achieve their health and wellness goals
+              Commitment reflected in stories of those who share their
+              experience
             </h1>
           </div>
         </InView>
@@ -174,7 +174,7 @@ export default async function Home() {
             <Reviews reviews={reviews} />
             <Button size="lg" asChild>
               <Link href="https://maps.app.goo.gl/9aVHWEXJ8E4rZ1zJ7">
-                Leave a review
+                More on Google Reviews
               </Link>
             </Button>
           </div>
@@ -184,75 +184,109 @@ export default async function Home() {
   );
 }
 
-const staticReviews = [
-  {
-    author_name: "Daniela Villalobos Torres",
-    author_url:
-      "https://www.google.com/maps/contrib/106329556966057521436/reviews",
-    language: "en",
-    original_language: "en",
-    profile_photo_url:
-      "https://lh3.googleusercontent.com/a/ACg8ocJn4LRiNRoXtG1Q6Ff4Tqb01LeUwFC-8V57vJNb20o9=s128-c0x00000000-cc-rp-mo",
-    rating: 5,
-    relative_time_description: "3 months ago",
-    text: "Great place to train with amazing staff; they also speak not only in Portuguese but English. And most important, they have a holistic vision between osteopathy and personalized training.",
-    time: 1700516149,
-    translated: false,
-  },
-  {
-    author_name: "Victor Hansen",
-    author_url:
-      "https://www.google.com/maps/contrib/111537559485101205749/reviews",
-    language: "en",
-    original_language: "en",
-    profile_photo_url:
-      "https://lh3.googleusercontent.com/a/ACg8ocKq2Htd0_QnmDxvyOAYSlUzn2dEsCVTrtEphoq_6zvxnw=s128-c0x00000000-cc-rp-mo",
-    rating: 5,
-    relative_time_description: "4 months ago",
-    text: "I've been working with Fabio for the past year and I can highly recommend his services. He speak excellent English and extremely knowledgeable. Initially I came to him to get help with a hip impingment which he resolved quickly. After this I've been using him a a strength coach 3x week with very good progress. He's the best PT I've ever worked with.",
-    time: 1698612373,
-    translated: false,
-  },
-  {
-    author_name: "Basilio Paz",
-    author_url:
-      "https://www.google.com/maps/contrib/110461696352716876082/reviews",
-    language: "en",
-    original_language: "en",
-    profile_photo_url:
-      "https://lh3.googleusercontent.com/a-/ALV-UjVHszmV2adNOgW4DkF3_0SOH2LiMC1j2yS2FEh83t7K3RL5=s128-c0x00000000-cc-rp-mo",
-    rating: 5,
-    relative_time_description: "3 months ago",
-    text: "I went for two days to Lisbon on a business trip and walked past Telo Studio. I was looking for a place to train in Lisbon in my stay and I couldn't be happier. Highly recommend this guys ! See you sooon ✨",
-    time: 1700308368,
-    translated: false,
-  },
-  {
-    author_name: "Manuela Luz",
-    author_url:
-      "https://www.google.com/maps/contrib/110132609227971670958/reviews",
-    language: "en",
-    original_language: "en",
-    profile_photo_url:
-      "https://lh3.googleusercontent.com/a-/ALV-UjXU0XGb4CwejTfwmt3vN5CtcJb0qgn51nAf7H_ehvGjB0Q=s128-c0x00000000-cc-rp-mo",
-    rating: 5,
-    relative_time_description: "4 months ago",
-    text: "I recently had an osteopathy session at TĒLO and I can honestly say it was a fantastic experience. The osteopath was not only highly skilled but also extremely empathetic and attentive. They took the time to thoroughly assess my issues and explained the treatment plan in detail, making me feel comfortable and informed.",
-    time: 1698433430,
-    translated: false,
-  },
-  {
-    author_name: "John Youden",
-    author_url:
-      "https://www.google.com/maps/contrib/114413764843608898540/reviews",
-    language: "en",
-    original_language: "en",
-    profile_photo_url:
-      "https://lh3.googleusercontent.com/a/ACg8ocIVFbrUr-uqOipL0pZc8ZHd3CkmyoIg0tJZ6G4ied30=s128-c0x00000000-cc-rp-mo",
-    rating: 5,
-    relative_time_description: "3 months ago",
-    text: "What Fabio does, for us lay people, can seem like magic. Somehow he knows what is ailing us, how it can be fixed, and then he proceeds to do just that, fix us. It comes from his deep understanding of how the body works, gathered from years of study and practice. That said, there’s still a little bit of magic happening there as well! Maestro!",
-    time: 1698786927,
-    translated: false,
-  },
-];
+const staticReviews = {
+  rating: 5,
+  userRatingCount: 30,
+  reviews: [
+    {
+      name: "places/ChIJ5d1lhqIzGQ0RqL36wcBymyY/reviews/ChZDSUhNMG9nS0VJQ0FnSUNsc2F5MElnEAE",
+      relativePublishTimeDescription: "3 months ago",
+      rating: 5,
+      text: {
+        text: "Great place to train with amazing staff; they also speak not only in Portuguese but English. And most important, they have a holistic vision between osteopathy and personalized training.",
+        languageCode: "en",
+      },
+      originalText: {
+        text: "Great place to train with amazing staff; they also speak not only in Portuguese but English. And most important, they have a holistic vision between osteopathy and personalized training.",
+        languageCode: "en",
+      },
+      authorAttribution: {
+        displayName: "Daniela Villalobos Torres",
+        uri: "https://www.google.com/maps/contrib/106329556966057521436/reviews",
+        photoUri:
+          "https://lh3.googleusercontent.com/a/ACg8ocJn4LRiNRoXtG1Q6Ff4Tqb01LeUwFC-8V57vJNb20o9=s128-c0x00000000-cc-rp-mo",
+      },
+      publishTime: "2023-11-20T21:35:49Z",
+    },
+    {
+      name: "places/ChIJ5d1lhqIzGQ0RqL36wcBymyY/reviews/ChZDSUhNMG9nS0VJQ0FnSUNGeFlLSklnEAE",
+      relativePublishTimeDescription: "4 months ago",
+      rating: 5,
+      text: {
+        text: "I've been working with Fabio for the past year and I can highly recommend his services. He speak excellent English and extremely knowledgeable. Initially I came to him to get help with a hip impingment which he resolved quickly. After this I've been using him a a strength coach 3x week with very good progress. He's the best PT I've ever worked with.",
+        languageCode: "en",
+      },
+      originalText: {
+        text: "I've been working with Fabio for the past year and I can highly recommend his services. He speak excellent English and extremely knowledgeable. Initially I came to him to get help with a hip impingment which he resolved quickly. After this I've been using him a a strength coach 3x week with very good progress. He's the best PT I've ever worked with.",
+        languageCode: "en",
+      },
+      authorAttribution: {
+        displayName: "Victor Hansen",
+        uri: "https://www.google.com/maps/contrib/111537559485101205749/reviews",
+        photoUri:
+          "https://lh3.googleusercontent.com/a/ACg8ocKq2Htd0_QnmDxvyOAYSlUzn2dEsCVTrtEphoq_6zvxnw=s128-c0x00000000-cc-rp-mo",
+      },
+      publishTime: "2023-10-29T20:46:13Z",
+    },
+    {
+      name: "places/ChIJ5d1lhqIzGQ0RqL36wcBymyY/reviews/ChZDSUhNMG9nS0VJQ0FnSUNsanZ6eWNnEAE",
+      relativePublishTimeDescription: "3 months ago",
+      rating: 5,
+      text: {
+        text: "I went for two days to Lisbon on a business trip and walked past Telo Studio. I was looking for a place to train in Lisbon in my stay and I couldn't be happier. Highly recommend this guys ! See you sooon ✨",
+        languageCode: "en",
+      },
+      originalText: {
+        text: "I went for two days to Lisbon on a business trip and walked past Telo Studio. I was looking for a place to train in Lisbon in my stay and I couldn't be happier. Highly recommend this guys ! See you sooon ✨",
+        languageCode: "en",
+      },
+      authorAttribution: {
+        displayName: "Basilio Paz",
+        uri: "https://www.google.com/maps/contrib/110461696352716876082/reviews",
+        photoUri:
+          "https://lh3.googleusercontent.com/a-/ALV-UjVHszmV2adNOgW4DkF3_0SOH2LiMC1j2yS2FEh83t7K3RL5=s128-c0x00000000-cc-rp-mo",
+      },
+      publishTime: "2023-11-18T11:52:48Z",
+    },
+    {
+      name: "places/ChIJ5d1lhqIzGQ0RqL36wcBymyY/reviews/ChZDSUhNMG9nS0VJQ0FnSUNGM1A3UUVREAE",
+      relativePublishTimeDescription: "4 months ago",
+      rating: 5,
+      text: {
+        text: "I recently had an osteopathy session at TĒLO and I can honestly say it was a fantastic experience. The osteopath was not only highly skilled but also extremely empathetic and attentive. They took the time to thoroughly assess my issues and explained the treatment plan in detail, making me feel comfortable and informed.",
+        languageCode: "en",
+      },
+      originalText: {
+        text: "I recently had an osteopathy session at TĒLO and I can honestly say it was a fantastic experience. The osteopath was not only highly skilled but also extremely empathetic and attentive. They took the time to thoroughly assess my issues and explained the treatment plan in detail, making me feel comfortable and informed.",
+        languageCode: "en",
+      },
+      authorAttribution: {
+        displayName: "Manuela Luz",
+        uri: "https://www.google.com/maps/contrib/110132609227971670958/reviews",
+        photoUri:
+          "https://lh3.googleusercontent.com/a-/ALV-UjXU0XGb4CwejTfwmt3vN5CtcJb0qgn51nAf7H_ehvGjB0Q=s128-c0x00000000-cc-rp-mo",
+      },
+      publishTime: "2023-10-27T19:03:50Z",
+    },
+    {
+      name: "places/ChIJ5d1lhqIzGQ0RqL36wcBymyY/reviews/ChdDSUhNMG9nS0VJQ0FnSUNGX2FUMjZ3RRAB",
+      relativePublishTimeDescription: "3 months ago",
+      rating: 5,
+      text: {
+        text: "What Fabio does, for us lay people, can seem like magic. Somehow he knows what is ailing us, how it can be fixed, and then he proceeds to do just that, fix us. It comes from his deep understanding of how the body works, gathered from years of study and practice. That said, there’s still a little bit of magic happening there as well! Maestro!",
+        languageCode: "en",
+      },
+      originalText: {
+        text: "What Fabio does, for us lay people, can seem like magic. Somehow he knows what is ailing us, how it can be fixed, and then he proceeds to do just that, fix us. It comes from his deep understanding of how the body works, gathered from years of study and practice. That said, there’s still a little bit of magic happening there as well! Maestro!",
+        languageCode: "en",
+      },
+      authorAttribution: {
+        displayName: "John Youden",
+        uri: "https://www.google.com/maps/contrib/114413764843608898540/reviews",
+        photoUri:
+          "https://lh3.googleusercontent.com/a/ACg8ocIVFbrUr-uqOipL0pZc8ZHd3CkmyoIg0tJZ6G4ied30=s128-c0x00000000-cc-rp-mo",
+      },
+      publishTime: "2023-10-31T21:15:27Z",
+    },
+  ],
+};
